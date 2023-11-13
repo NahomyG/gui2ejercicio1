408d87c29466ea8ee25c298de96e4d2285a855b9
NahomyG <114265537+NahomyG@users.noreply.github.com>
2023-11-13 16:20:25 -0600

    commit 5

---
+++ guia2ejercicio1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace guia2ejercicio1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingresa cuatro numeros");
+
+            Console.WriteLine("Escribe tu primer numero: ");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Escribe tu segundo numero: ");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Escribe el tercer numero: ");
+            double num3 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Escribe el cuarto numero: ");
+            double num4 = Convert.ToDouble(Console.ReadLine());
+
+            double suma = num1 + num2;
+
+            double producto = num3 + num4;
+
+            Console.WriteLine("El resultado del primer numero y segundo numero es: " + suma);
+            Console.WriteLine("El resultado del tercer numero con el cuarto numero es: " + producto);
+            Console.WriteLine("Gracias por usar el sistema (~°o°)~ ");
+
+            Console.ReadLine();
+        }
+    }
+}
